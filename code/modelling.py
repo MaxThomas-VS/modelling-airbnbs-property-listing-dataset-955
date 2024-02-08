8ce9668982788ea5f47f23db10c6457a76832a79
Max Thomas <maxthomas.vs@gmail.com>
2024-02-08 11:56:13 +0000

    Gets SGDregressor working.

--- code/modelling.py
+++ code/modelling.py
@@ -1,13 +1,30 @@
+#%%
 import argument_parser as ap
+import numpy as np 
+from sklearn.linear_model import SGDRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 from tabular_data import ModelData
+import torch
 
-args = ap.make_args()
-print(args)
 
-model_data = ModelData()
+# %%
 
-df = model_data.load_tabular_data(args['file_name'], args['import_path'])
 
-to_model = model_data.extract_label(df, 'Price_Night', numeric_only=True)
 
-print(to_model)
+if __name__ == '__main__':
+    args = ap.make_args()
+    print(args)
+
+    model_data = ModelData()
+
+    df = model_data.load_tabular_data(args['file_name'], args['import_path'])
+
+    to_model = model_data.extract_label(df, 'Price_Night', numeric_only=True)
+
+    X_train, X_test, y_train, y_test = train_test_split(to_model[0], to_model[1], test_size=0.2, random_state=1)
+
+    reg_mdl = SGDRegressor(max_iter=1000, tol=1e-3)
+    reg_mdl.fit(X_train, y_train)
+    print(reg_mdl.score(X_test, y_test))
