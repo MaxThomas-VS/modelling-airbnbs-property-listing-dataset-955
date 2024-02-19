c860f4426dfc846bc2b4b3178706ecdb5f778a53
Max Thomas <maxthomas.vs@gmail.com>
2024-02-19 11:36:28 +0000

    Adds looping over configs.

--- code/nn.py
+++ code/nn.py
@@ -8,6 +8,8 @@ from torchvision.transforms import PILToTensor
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 import yaml
+import random
+import os
 
 from tabular_data import ModelData
 
@@ -78,7 +80,7 @@ def train(model, config, epochs=10):
         for batch in train_loader:
             features, labels = batch
             predictions = model(features)
-            print(predictions[0:3])
+            #print(predictions[0:3])
             loss = F.mse_loss(predictions, labels)
             loss.backward() # populates gradients
             #print(loss.item())
@@ -145,20 +147,115 @@ class NN(torch.nn.Module):
 
 
 def get_nn_config(filename='nn_config_0.yaml'):
-    with open(filename, 'r') as file:
+    with open('nn_configs/' + filename, 'r') as file:
         config = yaml.safe_load(file)
     return config
 
+def generate_nn_config_range(filename='nn_configs/config_ranges.yaml'):
+    with open(filename, 'r') as file:
+        config_ranges = yaml.safe_load(file)
+    return config_ranges
+    
+     
+
+def generate_nn_configs(ranges='./nn_configs/config_ranges.yaml', export_path='./nn_configs/'):
+    config_ranges = generate_nn_config_range(ranges)
+    for ix in range(16):
+        config = {}
+        for key in config_ranges.keys():
+            try:
+                config[key] = random.choice(config_ranges[key])
+            except:
+                config[key] = config_ranges[key]
+        with open(export_path + 'nn_config_' + str(ix) + '.yaml', 'w') as file:
+            yaml.dump(config, file)
+    
+
+def plot_training_curve(train_loss, val_loss, title, show=False):
+    fig = plt.figure()
+    plt.plot(train_loss, label='train')
+    plt.plot(val_loss, label='val')
+    plt.legend()
+    plot_title = title + ' training curve'
+    plt.title(plot_title)
+    if show:
+        plt.show()
+    return fig
+
+def plot_validation_scatter(model, X_test, y_test, config_ext, test_loss, show=False):
+    fig = plt.figure()
+    tt = torch.tensor(X_test.values, dtype=torch.float32)
+    pred = model(tt)
+    pred = pred.detach().numpy()
+    plt.scatter(y_test,pred)
+    plot_title =  config_ext + ', test loss: ' + str(test_loss)
+    plt.title('Performance for ' + plot_title)
+    plt.xlabel('True')
+    plt.ylabel('Predicted')
+    if show:
+        plt.show()
+    return fig
+
+def get_test_loss(model, X_test, y_test):
+    test_data = AirBNBDataset(X_test, y_test)
+    test_loader = DataLoader(test_data, batch_size=len(test_data))
+    test_loss = []
+    for batch in test_loader:
+            features, labels = batch
+            predictions = model(features)
+            loss = F.mse_loss(predictions, labels)
+            test_loss.append(np.sqrt(loss.item()))
+    return np.mean(test_loss)
+
 #%%
     
+n_configs = 4
+epochs = 50
+
 X_train, X_val, X_test, y_train, y_val, y_test = data_for_nn()
     
 train_data = AirBNBDataset(X_train, y_train)
 val_data = AirBNBDataset(X_val, y_val)
+test_data = AirBNBDataset(X_test, y_test)
 
-train_loader = DataLoader(train_data, batch_size=216, shuffle=True)
-val_loader = DataLoader(val_data, batch_size=216)
+train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+val_loader = DataLoader(val_data, batch_size=64)
+test_loader = DataLoader(test_data, batch_size=64)
 
+
+configs = generate_nn_configs()
+for iconfig in range(n_configs):
+
+    print('Training model ' + str(1 + iconfig) + ' of ' + str(n_configs))
+    
+    config_ext = str(iconfig)
+    
+    config = get_nn_config('nn_config_' + config_ext + '.yaml')
+
+    model = NN()
+
+    test_loss_original = int(np.round(get_test_loss(model, X_test, y_test), 0))
+
+    print('Original test loss: ' + str(test_loss_original))
+
+    tl, vl = train(model, config, epochs=epochs)
+
+    test_loss_trained = int(round(get_test_loss(model, X_test, y_test), 0))
+    print('Trained test loss: ' + str(test_loss_trained))
+
+    f_train = plot_training_curve(tl, vl, config_ext)
+    f_val = plot_validation_scatter(model, X_test, y_test, config_ext, test_loss_trained)
+
+    # %%
+    results_dir = '../models/nn/' + config_ext + '/'
+    os.system('mkdir -p ' + results_dir)
+    torch.save(model.state_dict(), results_dir + 'nn.pt')
+    save_str = 'cp nn_configs/nn_config_'+config_ext+'.yaml ' + results_dir
+    os.system(save_str)
+    f_train.savefig(results_dir + 'training_curve.png')
+    f_val.savefig(results_dir + 'validation_scatter.png')
+
+    print('================ Done ================')
 
 #example = next(iter(train_loader))
 #print(example)
@@ -166,49 +263,46 @@ val_loader = DataLoader(val_data, batch_size=216)
 #features = features.type(torch.float32)
 ##features = features.reshape(batch_size, -1)
 #%%
-config = get_nn_config('nn_config_0.yaml')
-model = NN()
+#config_ext = 'base'
+#config = get_nn_config('nn_config_' + config_ext + '.yaml')
+#model = NN()
+#get_test_loss(model, X_test, y_test)
 
 #model = LinearRegression()
 #model(features)
-test_data = AirBNBDataset(X_test, y_test)
-test_loader = DataLoader(test_data, batch_size=len(test_data))
-for batch in test_loader:
-        features, labels = batch
-        predictions = model(features)
-        loss = F.mse_loss(predictions, labels)
-        print(np.sqrt(loss.item()))
+#test_data = AirBNBDataset(X_test, y_test)
+#test_loader = DataLoader(test_data, batch_size=len(test_data))
+#for batch in test_loader:
+#        features, labels = batch
+#        predictions = model(features)
+#        loss = F.mse_loss(predictions, labels)
+#        print(np.sqrt(loss.item()))
 #%%
 
-tl, vl = train(model, config, epochs=50)
+#tl, vl = train(model, config, epochs=50)
 
 #%%
-plt.plot(tl, label='train')
-plt.plot(vl, label='val')
-plt.legend()
-plt.ylim([0, 300])
+
 
 #%%
-test_data = AirBNBDataset(X_test, y_test)
-test_loader = DataLoader(test_data, batch_size=len(test_data))
-for batch in test_loader:
-        features, labels = batch
-        predictions = model(features)
-        loss = F.mse_loss(predictions, labels)
-        print(np.sqrt(loss.item()))
+#test_loss = int(round(get_test_loss(model, X_test, y_test), 0))
 
-tt = torch.tensor(X_test.values, dtype=torch.float32)
-pred = model(tt)
-pred = pred.detach().numpy()
-plt.scatter(y_test,pred)
+#f_train = plot_training_curve(tl, vl, config_ext)
+#f_val = plot_validation_scatter(model, X_test, y_test, config_ext, test_loss)
 
 # %%
-torch.save(model.state_dict(), 'test_model.pt')
+##torch.save(model.state_dict(), results_dir + 'nn.pt')
+#os.system('mkdir -p ' + results_dir)
+#save_str = 'cp nn_configs/nn_config_'+config_ext+'.yaml ' + results_dir
+####results_dir = '../models/nn/' + config_ext + '/'
+#os.system(save_str)
+#f_val.savefig(results_dir + 'validation_scatter.png')
+##f_train.savefig(results_dir + 'training_curve.png')
 
 # %%
-state_dict = torch.load('test_model.pt')
-new_model = NN()
-new_model.load_state_dict(state_dict)
-train(new_model, epochs=10)
+#state_dict = torch.load('test_model.pt')
+#new_model = NN()
+#new_model.load_state_dict(state_dict)
+#train(new_model, epochs=10)
 
 # %%
