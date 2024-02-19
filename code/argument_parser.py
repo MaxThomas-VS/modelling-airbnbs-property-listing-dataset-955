8b4653963493d65d4bc19c52a21d4d5089910772
Max Thomas <maxthomas.vs@gmail.com>
2024-02-19 12:01:36 +0000

    Add arguments for nn training.

--- code/argument_parser.py
+++ code/argument_parser.py
@@ -7,6 +7,8 @@ def make_args(parser=argparse.ArgumentParser()):
     parser.add_argument('--export_path', type=str, required=False, default='../data/intermediate/airbnb-property-listings/tabular_data/')
     parser.add_argument('--ml_model', type=comma_split_str, required=False, default=['SGDRegressor'])
     parser.add_argument('--random_state', type=int, required=False, default=100)
+    parser.add_argument('--nn_n_configs', type=int, required=False, default=16)
+    parser.add_argument('--nn_epochs', type=int, required=False, default=50)
     args = vars(parser.parse_args())
     args['script'] = parser.prog
     args['run_time'] = datetime.datetime.now()
