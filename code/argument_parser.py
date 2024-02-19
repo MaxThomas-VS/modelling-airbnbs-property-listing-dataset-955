ebe4bd003f48ab46a2c13f2dd978903976036097
Max Thomas <maxthomas.vs@gmail.com>
2024-02-19 14:16:11 +0000

    Can now train and eval all models from same script.

--- code/argument_parser.py
+++ code/argument_parser.py
@@ -1,5 +1,6 @@
 import datetime
 import argparse
+import ast
 
 def make_args(parser=argparse.ArgumentParser()):
     parser.add_argument('--file_name', type=str, required=False, default='listing.csv')
@@ -10,6 +11,9 @@ def make_args(parser=argparse.ArgumentParser()):
     parser.add_argument('--nn_n_configs', type=int, required=False, default=16)
     parser.add_argument('--nn_epochs', type=int, required=False, default=50)
     parser.add_argument('--batch_size', type=int, required=False, default=64)
+    parser.add_argument('--do_training', type=ast.literal_eval, required=False, default=True)
+    parser.add_argument('--do_evaluation', type=ast.literal_eval, required=False, default=True)
+
     args = vars(parser.parse_args())
     args['script'] = parser.prog
     args['run_time'] = datetime.datetime.now()
--- code/modelling.py
+++ code/modelling.py
@@ -14,6 +14,7 @@ from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import make_scorer
 import warnings
 from sklearn.exceptions import ConvergenceWarning
+import os
 
 
 
@@ -25,19 +26,30 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import joblib
 
-rstate=100
-pre_scale=True
+from nn import train_various_configurations
+
+
+def results_dir(args):
+    put_results_here = Path(args['export_path'], get_model_type(args['ml_model']))  
+    put_results_here = Path(put_results_here, args['ml_model'])
+    os.system('mkdir -p ' + str(put_results_here))
+    return put_results_here
 
 def save_model(best_params, args, extra_title='baseline_'):
+    
     model_name = extra_title + args['ml_model'] + '.joblib'
-    model_path = Path(args['export_path'], model_name)
+    
+    model_path = Path(results_dir(args), model_name)
+
     joblib.dump(best_params, model_path)
 
 def quality_control_figure(y_test, X_test, best_params, args):
     plt.figure()
     plt.scatter(y_test, best_params['best_estimator'].predict(X_test), color='black')
+    plt.ylabel('Predicted')
+    plt.xlabel('True')
     plt.title(args['ml_model'])
-    plt.savefig(Path(args['export_path'], args['ml_model']+'.png'))
+    plt.savefig(Path(results_dir(args), args['ml_model']+'.png'))
 
 def tune_regression_hyperparameters(model_class, X_train, y_train, param_grid):
 
@@ -60,22 +72,22 @@ def tune_regression_hyperparameters(model_class, X_train, y_train, param_grid):
 
 def tune_classification_hyperparameters(model_class, X_train, y_train, param_grid):
 
-    mse = make_scorer(mean_squared_error, greater_is_better=False)
-
+   # if model_class == 'LogisticRegression':
+   #     model = LogisticRegression(multi_class='multinomial', solver='newton-cg')
+    #pipe = make_pipeline(StandardScaler(), model_class(multi_class='multinomial', solver='saga'))
     pipe = make_pipeline(StandardScaler(), model_class())
 
     #search = GridSearchCV(pipe, param_grid, scoring=mse)
-    search = GridSearchCV(pipe, param_grid, scoring='neg_root_mean_squared_error', n_jobs=-1)
+    search = GridSearchCV(pipe, param_grid, n_jobs=-1)
 
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", category=ConvergenceWarning)
-        search.fit(X_train, y_train)
+    search.fit(X_train, y_train)
 
     search_results = {'best_estimator': search.best_estimator_,
                       'best_params': search.best_params_,
-                      'validation_RMSE': -search.best_score_}
+                      'validation_accuracy': -search.best_score_}
 
     return search_results
+
 
 def select_model(model_name):
     if model_name.lower() == 'sgdregressor':
@@ -105,8 +117,29 @@ def select_model(model_name):
     elif model_name.lower() == 'logisticregression':
         from sklearn.linear_model import LogisticRegression
         return LogisticRegression
+    elif model_name.lower() == 'decisiontreeclassifier':
+        from sklearn.tree import DecisionTreeClassifier
+        return DecisionTreeClassifier
+    elif model_name.lower() == 'randomforestclassifier':
+        from sklearn.ensemble import RandomForestClassifier
+        return RandomForestClassifier
+    elif model_name.lower() == 'gradientboostingclassifier':
+        from sklearn.ensemble import GradientBoostingClassifier
+        return GradientBoostingClassifier
     else:
         raise ValueError(f'{model_name} not a valid model name')
+    
+def get_model_type(model_name):
+    regressors = ['sgdregressor', 'decisiontreeregressor', 'randomforestregressor', 'gradientboostingregressor', 'svr', 'kernelridge', 'baysianridge']
+    classifiers = ['logisticregression', 'decisiontreeclassifier', 'randomforestclassifier', 'gradientboostingclassifier']
+    neural_networks = 'nn'
+    if model_name.lower() in regressors:
+        return 'regression'
+    elif model_name.lower() in classifiers:
+        return 'classification'
+    elif model_name.lower() == neural_networks:
+        return 'neural_network'
+    
 
 def make_parameter_grid(model_name):
     if model_name.lower() == 'sgdregressor':
@@ -171,9 +204,26 @@ def make_parameter_grid(model_name):
                 'catboostregressor__learning_rate': [0.1, 0.5, 1],
                 'catboostregressor__l2_leaf_reg': [1e-05, 0.0001, 0.01, 0.1]}
     elif model_name.lower() == 'logisticregression':
-        return {'logisticregression__l1_ratio': [0, 0.2, 0.4, 0.6, 0.8, 1],
-                'logisticregression__C': [0.1, 1, 10, 100]}
-
+        return {'logisticregression__penalty' : ['elastic_net','none'],
+                'logisticregression__C' : [0.001, 0.1, 1, 10],
+                'logisticregression__l1_ratio' : [0, 0.4, 0.7, 1]}
+    elif model_name.lower() == 'decisiontreeclassifier':
+        return {'decisiontreeclassifier__max_depth': [2, 6, 10],
+                'decisiontreeclassifier__min_samples_split': [2, 6, 10],
+                'decisiontreeclassifier__min_samples_leaf': [1, 4, 7, 10],
+                'decisiontreeclassifier__min_impurity_decrease': [0.0, 0.25, 0.5]}
+    elif model_name.lower() == 'randomforestclassifier':
+        return {'randomforestclassifier__n_estimators': [100, 300, 500],
+                'randomforestclassifier__max_depth': [2, 6, 10],
+                'randomforestclassifier__min_samples_split': [2, 6, 10],
+                'randomforestclassifier__min_samples_leaf': [1, 4, 7, 10],
+                'randomforestclassifier__min_impurity_decrease': [0.0, 0.25, 0.5]}
+    elif model_name.lower() == 'gradientboostingclassifier':    
+        return {'gradientboostingclassifier__n_estimators': [100, 300, 500],
+                'gradientboostingclassifier__max_depth': [3, 6, 9],
+                'gradientboostingclassifier__min_samples_split': [2, 6, 10],
+                'gradientboostingclassifier__min_samples_leaf': [1, 4, 7, 10],
+                'gradientboostingclassifier__min_impurity_decrease': [0.0, 0.25, 0.5]}
 
 def get_tabular_data(args, test_size=0.2, rstate=None, column='Price_Night', one_hot_encode_labels=False):
     if rstate is None:
@@ -193,7 +243,7 @@ def get_tabular_data(args, test_size=0.2, rstate=None, column='Price_Night', one
 
     return X_train, X_test, y_train, y_test
 
-def tune_one_model(args, X_train, y_train):
+def tune_one_regression_model(args, X_train, y_train):
 
     model_class = select_model(args['ml_model'])
 
@@ -218,21 +268,81 @@ def tune_one_model(args, X_train, y_train):
     print('finishd tuning model: ' + args['ml_model'])
 
 
-def evaluate_one_model(args, X_test, y_test):
+def tune_one_classification_model(args, X_train, y_train):
 
-    tuned_model = joblib.load(Path(args['export_path'], 'tuned_'+args['ml_model']+'.joblib'))
-    baseline_model = joblib.load(Path(args['export_path'], 'baseline_'+args['ml_model']+'.joblib'))
+    model_class = select_model(args['ml_model'])
+
+    # get baseline performance
+    if args['ml_model'] == 'LogisticRegression':
+        pipe = make_pipeline(StandardScaler(), model_class(multi_class='multinomial', solver='saga'))
+    else:
+        pipe = make_pipeline(StandardScaler(), model_class())
+    pipe.fit(X_train, y_train)
+    baseline_performance = {'best_estimator': pipe,
+                            'best_params': pipe.get_params()}
+    
+    save_model(baseline_performance, args, 'baseline_')
+    #print("baseline RMSE: %0.3f" % baseline_performance['validation_RMSE'])
+
+    # get best hyperparameters
+    # TODO: parameters should be passed as namelist rather tham function
+    parameters = make_parameter_grid(args['ml_model'])  
+    print(parameters)
+    best_params = tune_classification_hyperparameters(model_class, X_train, y_train, parameters)
+
+    save_model(best_params, args, 'tuned_')
+    #print("tuned RMSE (validation): %0.3f" % best_params['validation_RMSE'])
+    #print('tuned RMSE: '+ np.sqrt(mean_squared_error(y_test, best_params['best_estimator'].predict(X_test))))
+    print('finishd tuning model: ' + args['ml_model'])
+
+
+def evaluate_one_regression_model(args, X_test=None, y_test=None):
+
+    if X_test is None:
+        X_train, X_test, y_train, y_test = get_tabular_data(args)
+
+    #tuned_model = joblib.load(Path(args['export_path'], 'tuned_'+args['ml_model']+'.joblib'))
+    #baseline_model = joblib.load(Path(args['export_path'], 'baseline_'+args['ml_model']+'.joblib'))
+
+    tuned_model = joblib.load(Path(results_dir(args), 'tuned_' +args['ml_model'] + '.joblib'))
+    baseline_model = joblib.load(Path(results_dir(args), 'baseline_' +args['ml_model'] + '.joblib'))
 
     baseline_RMSE = np.sqrt(mean_squared_error(y_test, baseline_model['best_estimator'].predict(X_test)))
     tuned_RMSE = np.sqrt(mean_squared_error(y_test, tuned_model['best_estimator'].predict(X_test)))
-    
+        
     quality_control_figure(y_test, X_test, tuned_model, args)
 
     print('For ' + args['ml_model'])
     print(f'baseline RMSE: {baseline_RMSE}')
     print(f'tuned RMSE: {tuned_RMSE}')
+    print('')
 
     return tuned_RMSE
+
+
+def evaluate_one_classification_model(args, X_test=None, y_test=None):
+
+    if X_test is None:
+        X_train, X_test, y_train, y_test = get_tabular_data(args, column='Category')
+
+    #tuned_model = joblib.load(Path(args['export_path'], 'tuned_'+args['ml_model']+'.joblib'))
+    #baseline_model = joblib.load(Path(args['export_path'], 'baseline_'+args['ml_model']+'.joblib'))
+    tuned_model = joblib.load(Path(results_dir(args), 'tuned_' +args['ml_model'] + '.joblib'))
+    baseline_model = joblib.load(Path(results_dir(args), 'baseline_' +args['ml_model'] + '.joblib'))
+
+    baseline_accuracy = accuracy_score(y_test, baseline_model['best_estimator'].predict(X_test))
+    tuned_accuracy = accuracy_score(y_test, tuned_model['best_estimator'].predict(X_test))
+    
+    quality_control_figure(y_test, X_test, tuned_model, args)
+
+    print('For ' + args['ml_model'])
+    print(f'baseline accuracy: {baseline_accuracy}')
+    #print(baseline_model['best_estimator'].get_params())
+    print(f'tuned accuracy: {tuned_accuracy}')
+    print('')
+    #print(tuned_model['best_estimator'].get_params())
+
+    return tuned_accuracy
 
 def one_hot_encode_column(pds):
     #oh_encoder = OneHotEncoder(sparse_output=False, drop='first')
@@ -244,39 +354,67 @@ def one_hot_encode_column(pds):
     return np.argmax(oh_labels, axis=1)
     #return oh_labels
 
+def train_and_evaluate_regression_model(arguments):
+    
+        X_train, X_test, y_train, y_test = get_tabular_data(arguments)
+    
+        tune_one_regression_model(arguments, X_train, y_train)
+    
+        #model_stats = evaluate_one_regression_model(arguments, X_test, y_test)
+
+        #print(model_stats)
+
+        #return model_stats
+
+def train_and_evaluate_classification_model(arguments):
+    
+        X_train, X_test, y_train, y_test = get_tabular_data(arguments, column='Category')
+    
+        tune_one_classification_model(arguments, X_train, y_train)
+    
+        #model_stats = evaluate_one_classification_model(arguments, X_test, y_test)
+
+        #print(model_stats)
+
+        #return model_stats
+
+def train_and_evaluate_one_model(arguments):
+
+    if arguments['ml_model'].lower() == 'nn':
+        train_various_configurations(arguments)
+
+
+def do_training(arguments):
+    for model in arguments['ml_model']:
+        arguments['ml_model'] = model
+        if get_model_type(model) == 'regression':
+            train_and_evaluate_regression_model(arguments)
+        elif get_model_type(model) == 'classification':
+            train_and_evaluate_classification_model(arguments)
+        elif get_model_type(model) == 'neural_network':
+            train_and_evaluate_one_model(arguments)
+
+def do_evaluation(arguments):
+    for model in arguments['ml_model']:
+        arguments['ml_model'] = model
+        if get_model_type(model) == 'regression':
+            evaluate_one_regression_model(arguments)
+        elif get_model_type(model) == 'classification':
+            evaluate_one_classification_model(arguments)
+        # model evaluation is done alongside tuning for nn
+    
+
 #%%
 if __name__ == '__main__':
     
     arguments = ap.make_args()
-        
-    X_train, X_test, y_train, y_test = get_tabular_data(arguments)
-    
-    model_stats = {}
+    print(arguments)
 
-    for model in arguments['ml_model']:
+    if arguments['do_training']:
+        do_training(arguments)
 
-        arguments['ml_model'] = model
-
-        tune_one_model(arguments, X_train, y_train)
-
-        model_stats[model] = evaluate_one_model(arguments, X_test, y_test)
-
-    print(model_stats)
-    print('See also quality control figures in '+arguments['export_path'])
-
-
-    # rough logisitc regression
-    model = LogisticRegression(multi_class='multinomial', solver='newton-cg')
-    X_train, X_test, y_train, y_test = get_tabular_data(arguments, column='Category', one_hot_encode_labels=True)
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    print(classification_report(y_test, y_pred))
-
-    parmaeters = {'penalty': ['l1','l2','none'], 'C': [0.001,0.01,0.1,1,10,100,1000]}
-    search = GridSearchCV(model, param_grid=parmaeters, n_jobs=-1)
-    search.fit(X_train, y_train)
-    y_pred_tuned = search.best_estimator_.predict(X_test)
-    print(classification_report(y_test, y_pred_tuned))
+    if arguments['do_evaluation']:
+        do_evaluation(arguments)
 
 
 
